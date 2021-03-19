35e56c970a4ba9535ccb04d4915e16c5ba759446
MaikolCode <maikol320434@gmail.com>
2021-03-18 20:54:52 -0500

    Iniciando con REACT

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          MICHAEL STIVEN PARRA
         </p>
+        <code>src/App.js</code>
         <a
           className="App-link"
           href="https://reactjs.org"
